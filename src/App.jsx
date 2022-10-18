f712cd8dc6a57773b339b15ba98f54877e0e5e69
portableclass <av.yashutkin@gmail.com>
2022-10-19 00:25:11 +0300

    0.01

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './assets/styles/scss/app.scss';
+
+function App() {
+    return (
+        <div className='app-wrapper'>
+            <h1>React Snippet</h1>
+        </div>
+    );
+}
+
+export default App;
